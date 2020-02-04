4b8b1ecc12f60a5edd00af53fa25e776b0e6f655
Joel Gamble <jjoelgamble@gmail.com>
2020-02-03 21:12:00 -0600

    added gulp and ported over some styles from january. Need to work on creating layouts and modifying current posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,14 @@
 const { DateTime } = require("luxon");
 const fs = require("fs");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
-const pluginSass = require("eleventy-plugin-sass");
+
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
-  eleventyConfig.addPlugin(pluginSass, sassPluginOptions);
 
   eleventyConfig.setDataDeepMerge(true);
 
---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp    = require("gulp");
+const sass    = require("gulp-sass");
+
+
+
+/*
+  generate the css with sass
+*/
+gulp.task('css', function() {
+  return gulp.src('./src/scss/main.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    })
+    .on('error', sass.logError))
+    .pipe(gulp.dest('_site/css'));
+});
+
+
+
+
+
+/*
+  Watch folders for changess
+*/
+gulp.task("watch", function() {
+  gulp.watch('./src/scss/**/*.scss', gulp.parallel('css'));
+});
+
+
+/*
+  Let's build this sucker.
+*/
+gulp.task('build', gulp.parallel(
+  'css'
+));
