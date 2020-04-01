c082dc5bd8821bdd71e05dd42395b957e39c32cd
Joel Gamble <jjoelgamble@gmail.com>
2020-03-31 20:57:46 -0500

    march update

--- src/js/march.js
+++ src/js/march.js
@@ -2,22 +2,27 @@
 
 var articles = document.querySelectorAll('.article');
 console.log(articles);
-articleHeights = []
-for(i = 0; i < articles.length; i ++){
-  if(articles[i].classList.contains('featured-article')){
-    console.log('is featured');
-  } else {
-    console.log(articles[i].clientHeight);
-    articleHeights.push(articles[i].clientHeight);
-  }
-  console.log(articleHeights);
 
-  for(j = 0; j < articles.length; j++) {
-    if(articles[j].classList.contains('featured-article')){
-      // break;
+function setArticleHeight() {
+  articleHeights = []
+  for(i = 0; i < articles.length; i ++){
+    if(articles[i].classList.contains('featured-article')){
+      console.log('is featured');
     } else {
-      articles[j].style.height = Math.max.apply(Math, articleHeights) + 'px';
-      console.log(Math.max.apply(Math, articleHeights) + 'px');
+      console.log(articles[i].clientHeight);
+      articleHeights.push(articles[i].clientHeight);
+    }
+    console.log(articleHeights);
+
+    for(j = 0; j < articles.length; j++) {
+      if(articles[j].classList.contains('featured-article')){
+        // break;
+      } else {
+        articles[j].style.height = Math.max.apply(Math, articleHeights) + 'px';
+        console.log(Math.max.apply(Math, articleHeights) + 'px');
+      }
     }
   }
 }
+document.addEventListener('resize', setArticleHeight);
+window.addEventListener('load', setArticleHeight);
